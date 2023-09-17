d63aab80eeb0487211235ae9664519e7c29116b0
Sumon <m.sumon@outlook.com>
2023-09-17 20:53:01 +0600

    validation added

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,11 +1,28 @@
 import express from 'express';
 import { AcademicSemesterController } from './academicSemester.controller';
+import validateRequest from '../../middlewares/validateRequest';
+import { AcademicSemesterValidation } from './academicSemester.validation';
+import { ENUM_USER_ROLE } from '../../../enums/user';
+import auth from '../../middlewares/auth';
 const router = express.Router();
 
-router.patch('/:id', AcademicSemesterController.updateAcaSem);
-router.post('/', AcademicSemesterController.insertIntoDB);
 router.get('/', AcademicSemesterController.getAllFromDB);
 router.get('/:id', AcademicSemesterController.getAcaSemById);
+
+router.post(
+  '/',
+  validateRequest(AcademicSemesterValidation.create),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.insertIntoDB
+);
+
+router.patch(
+  '/:id',
+  validateRequest(AcademicSemesterValidation.update),
+  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+  AcademicSemesterController.updateAcaSem
+);
+
 router.delete('/:id', AcademicSemesterController.deleteAcaSem);
 
 export const AcademicSemesterRoutes = router;
