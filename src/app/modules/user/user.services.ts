e3b77829b606a7488221d24939771340366e038d
Sumon <m.sumon@outlook.com>
2023-09-20 17:18:49 +0600

    aca dem validation added

--- src/app/modules/user/user.services.ts
+++ src/app/modules/user/user.services.ts
@@ -1,11 +1,22 @@
 import { Request } from 'express';
 import { FileUploadHelper } from '../../../helpers/fileUploadHelper';
 import { ICloudinaryResponse, IUploadFile } from '../../../interfaces/file';
+import { AuthService } from '../../../axios';
 
 const createStudent = async (req: Request) => {
   const file = req.file as IUploadFile;
-  const uploadedImage:ICloudinaryResponse = await FileUploadHelper.uploadToCloudynary(file);
-  console.log(file, uploadedImage);
+  const uploadedImage = await FileUploadHelper.uploadToCloudynary(file);
+
+  if (uploadedImage) {
+    req.body.profileImage = uploadedImage.secure_url;
+  }
+  const { academicDepartment } = req.body;
+  const academicDepartmentResponse = await AuthService.get(
+    `/academic-departments?syncId=${academicDepartment}`
+  );
+  if (academicDepartmentResponse.data && Array.isArray(academicDepartmentResponse.data)) {
+    req.body.academicDepartment = academicDepartmentResponse.data[0].id;
+  }
 };
 
 export const UserService = { createStudent };
